c0df5028e4afb39fdcca829ce18e87c9b266f42b
Benjamin Wild <b.w@fu-berlin.de>
2020-10-31 10:13:46 +0100

    Ignore pylint false positive

--- unsupervised_behaviors/utils.py
+++ unsupervised_behaviors/utils.py
@@ -14,6 +14,8 @@ def increase_histogram(hist: np.array, image: np.array, selection: np.array):
         selection np.array(shape=(H, W), dtype=np.bool):
             Image mask for pixels that should be used.
     """
+    # Ignore false positive, see: github.com/PyCQA/pylint/issues/2910
+    # pylint: disable=not-an-iterable
     for x in numba.prange(image.shape[0]):
         for y in numba.prange(image.shape[1]):
             value = image[x, y]
